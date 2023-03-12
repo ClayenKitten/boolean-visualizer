bb99698be0488fd3acbf79ccc8a5197cea1b0a1c
LowLevelVirtualMan <dima070704@yandex.ru>
2023-03-12 03:54:18 +0300

    Refactor svg_gen

--- src/svg_gen.rs
+++ src/svg_gen.rs
@@ -1,10 +1,7 @@
-use std::{f64::consts::FRAC_PI_2, iter::repeat};
+use std::f64::consts::FRAC_PI_2;
 
 use svg::{
-    node::{
-        element::{path::Data, Circle, Group, Path, Pattern, Rectangle, Text},
-        Value,
-    },
+    node::element::{path::Data, Circle, Group, Path, Pattern, Rectangle, Text},
     Document, Node,
 };
 
@@ -12,13 +9,10 @@ pub fn generate<F>(vars: &[char], func: F) -> svg::Document
 where
     F: Fn(&[bool]) -> bool,
 {
-    let mut document = Document::new()
+    let document = Document::new()
         .set("viewBox", (0, 0, 100, 100))
-        .add(fill_pattern());
-    let all_false = repeat(false)
-        .take(vars.len())
-        .collect::<Vec<bool>>();
-    document = document.add(background(func(&all_false)));
+        .add(fill_pattern())
+        .add(background(func(&vec![false; vars.len()])));
     match vars.len() {
         0 => document,
         1 => {
@@ -86,71 +80,65 @@ fn background(filled: bool) -> impl Node {
         .set("fill", if filled { "url(#hatch)" } else { "white" })
 }
 
-fn single(var: char, filled: bool) -> impl Node {
+const RADIUS: f64 = 25.;
+
+fn single(var: char, fill: bool) -> impl Node {
     Group::new()
-        .add(circle("50", "50", "25", filled))
-        .add(text(50., 50., var))
+        .set("transform", "translate(50, 50)")
+        .add(circle(Pos::ZERO, fill))
+        .add(text(0., 0., var))
 }
 
 fn double(vars: [char; 2], fill: [bool; 3]) -> impl Node {
-    Group::new()
-        .add(circle("33.33", "50", "25", fill[0]))
-        .add(circle("66.66", "50", "25", fill[2]))
-        .add(
-            intersection(
-                Pos { x: 100. / 3., y: 50. },
-                Pos { x: 200. / 3., y: 50. },
-                25.,
-                fill[1],
-            )
-        )
-        .add(text(100. / 3., 50., vars[0]))
-        .add(text(200. / 3., 50., vars[1]))
-}
-
-fn triple(vars: [char; 3], fill: [bool; 7]) -> impl Node {
-    let r = 25.;
-    let dy = r * f64::sqrt(3.) / 6.;
-
-    let c1 = Pos { x: 0., y: -2. * dy };
-    let c2 = Pos { x: -r / 2., y: dy };
-    let c3 = Pos { x: r / 2., y: dy };
+    let c1 = Pos { x: -RADIUS / 2., y: 0. };
+    let c2 = Pos { x: RADIUS / 2., y: 0. };
 
     Group::new()
         .set("transform", "translate(50, 50)")
-        .add(circle(c1.x, c1.y, r, fill[0]))
-        .add(circle(c2.x, c2.y, r, fill[1]))
-        .add(circle(c3.x, c3.y, r, fill[2]))
-        .add(intersection(c1, c2, r, fill[3]))
-        .add(intersection(c1, c3, r, fill[4]))
-        .add(intersection(c2, c3, r, fill[5]))
-        .add(center(r, fill[6]))
+        .add(circle(c1, fill[0]))
+        .add(circle(c2, fill[2]))
+        .add(intersection2(c1, c2, fill[1]))
+        .add(text(-RADIUS * (15. / 16.), 0., vars[0]))
+        .add(text(RADIUS * (15. / 16.), 0., vars[1]))
+}
+
+fn triple(vars: [char; 3], fill: [bool; 7]) -> impl Node {
+    let dy = RADIUS * f64::sqrt(3.) / 6.;
+
+    let c1 = Pos { x: 0., y: -2. * dy };
+    let c2 = Pos { x: -RADIUS / 2., y: dy };
+    let c3 = Pos { x: RADIUS / 2., y: dy };
+
+    Group::new()
+        .set("transform", "translate(50, 50)")
+        .add(circle(c1, fill[0]))
+        .add(circle(c2, fill[1]))
+        .add(circle(c3, fill[2]))
+        .add(intersection2(c1, c2, fill[3]))
+        .add(intersection2(c1, c3, fill[4]))
+        .add(intersection2(c2, c3, fill[5]))
+        .add(intersection3(fill[6]))
         .add(text(c1.x, c1.y - 12., vars[0]))
         .add(text(c2.x - 9., c2.y + 9., vars[1]))
         .add(text(c3.x + 9., c3.y + 9., vars[2]))
 }
 
-fn circle(
-    cx: impl Into<Value>,
-    cy: impl Into<Value>,
-    r: impl Into<Value>,
-    fill: bool
-) -> Circle {
+fn circle(center: Pos, fill: bool) -> Circle {
     Circle::new()
-        .set("cx", cx)
-        .set("cy", cy)
-        .set("r", r)
+        .set("cx", center.x)
+        .set("cy", center.y)
+        .set("r", RADIUS)
         .set("stroke", "black")
         .set("stroke-width", 1)
         .set("fill", if fill { "url(#hatch)" } else { "white" })
 }
 
-fn intersection(circle1: Pos, circle2: Pos, radius: f64, fill: bool) -> Path {
+fn intersection2(circle1: Pos, circle2: Pos, fill: bool) -> Path {
     let distance = Pos::distance(circle1, circle2);
     let center = Pos::center(circle1, circle2);
     let angle = (circle1.y - circle2.y).atan2(circle1.x - circle2.x) + FRAC_PI_2;
     // Length of rhombus's diagonal by its side and another diagonal
-    let length = f64::sqrt(4. * radius.powi(2) - distance.powi(2));
+    let length = f64::sqrt(4. * RADIUS.powi(2) - distance.powi(2));
     let start = {
         let x = center.x - (length / 2.) * angle.cos();
         let y = center.y - (length / 2.) * angle.sin();
@@ -163,35 +151,35 @@ fn intersection(circle1: Pos, circle2: Pos, radius: f64, fill: bool) -> Path {
             "d",
             Data::new()
                 .move_to((start.x, start.y))
-                .elliptical_arc_by((radius, radius, 0, 0, 0, dx, dy))
-                .elliptical_arc_to((radius, radius, 0, 0, 0, start.x, start.y)),
+                .elliptical_arc_by((RADIUS, RADIUS, 0, 0, 0, dx, dy))
+                .elliptical_arc_to((RADIUS, RADIUS, 0, 0, 0, start.x, start.y)),
         )
         .set("fill", if fill { "url(#hatch)" } else { "white" })
         .set("stroke", "black")
         .set("stroke-width", 1)
 }
 
-fn center(radius: f64, fill: bool) -> Path {
+fn intersection3(fill: bool) -> Path {
     let pos1 = Pos {
         x: 0.,
-        y: -f64::sqrt((radius / 2.).powi(2) + (3f64.sqrt() / 6. * radius).powi(2)),
+        y: -f64::sqrt((RADIUS / 2.).powi(2) + (3f64.sqrt() / 6. * RADIUS).powi(2)),
     };
     let pos2 = Pos {
-        x: radius / 2.,
-        y: 3f64.sqrt() * radius / 6.,
+        x: RADIUS / 2.,
+        y: 3f64.sqrt() * RADIUS / 6.,
     };
     let pos3 = Pos {
-        x: -radius / 2.,
-        y: 3f64.sqrt() * radius / 6.,
+        x: -RADIUS / 2.,
+        y: 3f64.sqrt() * RADIUS / 6.,
     };
     Path::new()
         .set(
             "d",
             Data::new()
                 .move_to((pos1.x, pos1.y))
-                .elliptical_arc_to((radius, radius, 0, 0, 1, pos2.x, pos2.y))
-                .elliptical_arc_to((radius, radius, 0, 0, 1, pos3.x, pos3.y))
-                .elliptical_arc_to((radius, radius, 0, 0, 1, pos1.x, pos1.y)),
+                .elliptical_arc_to((RADIUS, RADIUS, 0, 0, 1, pos2.x, pos2.y))
+                .elliptical_arc_to((RADIUS, RADIUS, 0, 0, 1, pos3.x, pos3.y))
+                .elliptical_arc_to((RADIUS, RADIUS, 0, 0, 1, pos1.x, pos1.y)),
         )
         .set("fill", if fill { "url(#hatch)" } else { "white" })
         .set("stroke", "black")
@@ -232,6 +220,8 @@ struct Pos {
 }
 
 impl Pos {
+    pub const ZERO: Pos = Pos { x: 0., y: 0. };
+
     /// Computes the distance between points.
     pub fn distance(self, other: Self) -> f64 {
         f64::sqrt((self.x - other.x).powi(2) + (self.y - other.y).powi(2))
