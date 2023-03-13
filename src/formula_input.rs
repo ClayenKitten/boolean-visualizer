2b5221b51143e358959523d265bbabde2ce88c21
LowLevelVirtualMan <dima070704@yandex.ru>
2023-03-13 03:06:08 +0300

    Update syntax highlightning
    Paired brackets and colored variables

--- src/formula_input.rs
+++ src/formula_input.rs
@@ -71,13 +71,59 @@ impl Component for FormulaInput {
 }
 
 fn highlighting(input: &str) -> Html {
-    let highlighted = input
-        .chars()
-        .map(|ch| match ch {
-            '&' | '|' | '!' => html!(<span class="operator">{ch}</span>),
-            '(' | ')' => html!(<span class="bracket">{ch}</span>),
-            'a'..='z' | '0' | '1' => html!(<span class="variable">{ch}</span>),
-            _ => html!(<span class="error">{ch}</span>),
+    enum Entry {
+        Variable(char),
+        Operator(char),
+        Bracket {
+            depth: i32,
+            is_left: bool,
+        },
+        Unknown(char),
+    }
+
+    let mut buffer = Vec::<Entry>::with_capacity(input.len());
+    let mut depth = 0i32;
+    for ch in input.chars() {
+        buffer.push(match ch {
+            'a'..='z' | '0' | '1'
+                => Entry::Variable(ch),
+            '&' | '|' | '!'
+                => Entry::Operator(ch),
+            '(' => {
+                let entry = Entry::Bracket { depth, is_left: true };
+                depth += 1;
+                entry
+            },
+            ')' => {
+                depth -= 1;
+                Entry::Bracket { depth, is_left: false }
+            },
+            _ => Entry::Unknown(ch),
+        })
+    }
+    let highlighted = buffer.into_iter()
+        .map(|entry| match entry {
+            Entry::Variable(var)
+                => html!(<span class="variable">{var}</span>),
+            Entry::Operator(op)
+                => html!(<span class="operator">{op}</span>),
+            Entry::Bracket { depth: mut cur_depth, is_left } => {
+                let invalid = cur_depth < 0 ||
+                    is_left && cur_depth < depth;
+                if depth < 0 {
+                    cur_depth += -depth;
+                }
+                html! {
+                    <span
+                        class={if invalid {"bracket error"} else {"bracket"}}
+                        data-depth={(cur_depth % 3).abs().to_string()}
+                    >
+                        {if is_left {'('} else {')'}}
+                    </span>
+                }
+            }
+            Entry::Unknown(ch)
+                => html!(<span class="error">{ch}</span>),
         })
         .collect::<Html>();
     html!(<code>{highlighted}</code>)
