38bbd9508745f072e158e07248ef415d6925ffc8
ClayenKitten <68070406+ClayenKitten@users.noreply.github.com>
2023-03-13 00:35:17 +0000

    Update gh-pages to output generated at 570fa30

--- boolean-visualizer-c5699030e553244f.js
+++ boolean-visualizer-c5699030e553244f.js
@@ -571,11 +571,11 @@ function getImports() {
     imports.wbg.__wbg_removeAttribute_ad7a5bf2eed30373 = function() { return handleError(function (arg0, arg1, arg2) {
         getObject(arg0).removeAttribute(getStringFromWasm0(arg1, arg2));
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper342 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper346 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 21, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper962 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper967 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 21, __wbg_adapter_19);
         return addHeapObject(ret);
     };
@@ -614,7 +614,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('boolean-visualizer-8007d5494fb13d3b_bg.wasm', import.meta.url);
+        input = new URL('boolean-visualizer-c5699030e553244f_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
